9017dbe0ce9d8b57098c30ab8d0ec861dc66d9ab
babuandres <babuandres@hotmail.com>
2015-03-04 16:54:01 -0500

    Subida real de OvernightPackage

---
+++ AppServicioEntregaDePaquetes/Modelo/OvernightPackage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppServicioEntregaDePaquetes.Modelo
+{
+    public class OvernightPackage : Package
+    {
+
+
+
+    }
+}
