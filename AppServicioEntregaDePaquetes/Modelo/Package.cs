81868e2a2ad95fe64a30a6e4886abb9e29065411
babuandres <babuandres@hotmail.com>
2015-03-04 15:53:31 -0500

    Clase Padre Completa

--- AppServicioEntregaDePaquetes/Modelo/Package.cs
+++ AppServicioEntregaDePaquetes/Modelo/Package.cs
@@ -59,13 +59,72 @@ namespace AppServicioEntregaDePaquetes.Modelo
             get { return codigoPostalDestinatario; }
             set { codigoPostalDestinatario = value; }
         }
-
         #endregion
 
         #region "Constructores"
+        public Package() { 
+        ///<summary>
+        ///Contructor de clase Package
+        ///</summary>
+        ///
+            this.codigo = "1.234.567.890";
+            this.nombre = "Nombre De La Persona";
+            this.direccion = "Direccion del Destinatario";
+            this.ciudad = "Ciudad Destino";
+            this.departamento = "Despartamento Destino";
+            this.codigoPostalRemitente = "Código postal Remitente";
+            this.codigoPostalDestinatario = "Código Postal Destinatario";
+        }
+        ///<summary>
+        ///Contructor de inicialización de Personas
+        ///</summary>
+        ///
+        public Package(string codigo, string nombre, string direccion, string ciudad, string departamento, string codigoPostalRemitente,string codigoPostalDestinatario) {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.ciudad = ciudad;
+            this.departamento = departamento;
+            this.codigoPostalRemitente = codigoPostalRemitente;
+            this.codigoPostalDestinatario = codigoPostalDestinatario;
+        
+        }
         #endregion
 
         #region"Medotodos Sobreescritos"
+
+        public override string ToString()
+        {
+            return "Codigo:  " + this.codigo + "\n" +
+                     "Nombre: " + this.nombre + "\n" +
+                     "Direccion Destino: " + this.direccion + "\n" +
+                     "Ciudad Destino: " + this.ciudad + "\n" +
+                     "Departamento: " + this.departamento + "\n" +
+                     "Codigo Postal del Remitente" + this.codigoPostalRemitente + "\n" +
+                     "Codigo Postal del Destinanatio" + this.codigoPostalDestinatario;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Package p = (Package)obj;
+            bool result = false;
+            if ((this.codigo == p.codigo) &&
+                (this.nombre == p.nombre) &&
+                (this.direccion == p.direccion) &&
+                (this.ciudad == p.ciudad) &&
+                (this.departamento == p.departamento) &&
+                (this.codigoPostalRemitente == p.codigoPostalRemitente) &&
+                (this.codigoPostalDestinatario == p.codigoPostalDestinatario))
+                result = true;
+
+                return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
         #endregion
     }
 }
