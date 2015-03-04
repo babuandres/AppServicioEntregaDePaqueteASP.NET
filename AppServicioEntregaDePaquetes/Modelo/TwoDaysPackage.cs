dd937c3f40efcf87dc66f73ab764cf67833fe91e
babuandres <babuandres@hotmail.com>
2015-03-04 16:31:28 -0500

    Primera Subida, Clase Hija TwoDaysPackage

---
+++ AppServicioEntregaDePaquetes/Modelo/TwoDaysPackage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppServicioEntregaDePaquetes.Modelo
+{
+    public class TwoDaysPackage : Package
+    {
+        #region "Atributos"
+        private double cuotaFija;        
+        #endregion
+
+        #region "Propiedades"
+        public double CuotaFija
+        {
+            get { return cuotaFija; }
+            set { cuotaFija = value; }
+        }
+        #endregion
+
+        #region "Constructores"
+        
+        public TwoDaysPackage() : base ()
+        {
+
+            this.cuotaFija = 0.0;
+        }
+         public TwoDaysPackage (string codigo, string nombre, string direccion, string ciudad, string departamento, string codigoPostalRemitente,string codigoPostalDestinatario, double peso, double costoPorGr, double valorTotal, double cuotaFija)
+            : base(codigo, nombre, direccion, ciudad, departamento, codigoPostalRemitente, codigoPostalDestinatario, peso, costoPorGr, valorTotal)
+        {
+            this.cuotaFija = cuotaFija;
+        }
+        #endregion
+
+
+
+
+    }
+}
